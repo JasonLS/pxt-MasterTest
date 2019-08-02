a4412f02140e4b9c3773a17bffe99f209937639a
JasonLS <jsedluk01@gmail.com>
2019-08-02 11:20:13 -0400

    Updates.

---
+++ Custom.ts
@@ -0,0 +1,23 @@
+namespace robot {
+    export class Robot {
+        constructor() { }
+        //% blockId="gigglebot_stop_motor" block="Stop %which motor |"
+        move() {
+
+            // do nothing – don’t crash!
+
+        }
+
+        light() {
+
+            return 0;
+
+        }
+
+    }
+
+
+
+    export let currentRobot: Robot; // ß libraries can set this field when loading
+
+}
