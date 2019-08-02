1da8baf4e196728fdd51cb9327a361cc394d2226
JasonLS <jsedluk01@gmail.com>
2019-08-02 11:44:01 -0400

    Updates.

--- Custom.ts
+++ Custom.ts
@@ -1,7 +1,9 @@
+//% weight=10 color=#006400 icon="\uf1b9" block="Tinybit"
 namespace robot {
     export class Robot {
         constructor() { }
-        //% blockId="gigglebot_stop_motor" block="Stop %which motor |"
+
+        //% blockId="Test" block="Play Music"
         move() {
 
             // do nothing – don’t crash!
