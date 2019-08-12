61496123f109f8fef457d3a67562353e68014808
JasonLS <jsedluk01@gmail.com>
2019-08-12 10:09:11 -0400

    Updates.

--- main.ts
+++ main.ts
@@ -1,15 +1,1 @@
-//% weight=10 color=#006400 icon="\uf1b9" block="Tinybit"
-namespace robot {
-    export class Robot {
-        constructor() { }
-
-        //% blockId="Test" block="Play Music"
-        move(): void {
-
-            // do nothing – don’t crash!
-
-        }
-
-    }
-    export let currentRobot: Robot; //libraries can set this field when loading
-}
+ 
